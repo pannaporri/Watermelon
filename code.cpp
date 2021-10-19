164883404db7d4e4126114b6660162c3bf259a5a
pranjalraj <56797788+pranjalraj@users.noreply.github.com>
2021-10-19 20:11:42 +0530

    Update code.cpp
    the previous code was also correct. maybe there was some other issue while executing it. try this code please. it should work.

--- code.cpp
+++ code.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int w;
-    cin>>w;
-    if(w%2==0 && w>2){
-        cout<<"YES"<<endl;
-    }
-    else{
-        cout<<"NO"<<endl;
-    }
+
+int main(){
+    int w = 0; cin >> w;
+    string output = (w <= 2 || w % 2) ? "NO" : "YES";
+    cout << output << endl;
     return 0;
 }
-//this code does not pass all the test cases
